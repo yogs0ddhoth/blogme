b37441625f76646c4e80d9c2ec022c789c00e214
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-04-09 11:12:57 -0400

    relationships extablished

---
+++ models/index.js
@@ -0,0 +1,29 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+});
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'author_id',
+  onDelete: 'CASCADE'
+});
+Comment.belongsTo(User, {
+  foreignKey: 'author_id',
+});
+
+module.exports = { User, Post, Comment };
