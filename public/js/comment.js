9a6b1a6c6faf0fe8561c201e27c2b62d6b53456b
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-04-09 11:14:24 -0400

    post comment

---
+++ public/js/comment.js
@@ -0,0 +1,27 @@
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+  console.log('test1');
+  const comment = document.querySelector('#comment-input').value.trim();
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+
+  if (comment && id) {
+    console.log('test2');
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'POST',
+      body: JSON.stringify({ comment }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document
+  .querySelector('.comment-form')
+  .addEventListener('submit', signupFormHandler);
