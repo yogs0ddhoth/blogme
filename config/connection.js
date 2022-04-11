2365f9a5969bb59e8975b1944f4e0e3fbeb7b6c3
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-04-11 17:16:43 -0400

    heroku config

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
+const{ HOST, USER, PASSWORD, DB } = require('./db.config'); 
 
 let sequelize;
 
@@ -7,13 +8,13 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    DB,
+    USER,
+    PASSWORD,
     {
-      host: 'localhost',
+      host: HOST,
       dialect: 'mysql',
-      port: 3306
+      // port: 3306
     }
   );
 }
---
+++ config/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  HOST: "us-cdbr-east-05.cleardb.net",
+  USER: "b32ee77ece0dcc",
+  PASSWORD: "4225651c",
+  DB: "heroku_8c59a616683e72e"
+};
